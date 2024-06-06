b37164d05e89078fd67da5f1cc4dcd8118770689
韩浩宇 <hyskr0620@gmail.com>
2024-06-06 21:41:13 +0800

    add scale slider

--- src/PdfToPptxConverter.jsx
+++ src/PdfToPptxConverter.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Document, pdfjs } from 'react-pdf';
 import PptxGenJS from 'pptxgenjs';
-import { Upload, message, Progress } from 'antd';
+import { Upload, message, Progress, Slider, Row, Col, InputNumber } from 'antd';
 import { InboxOutlined } from '@ant-design/icons';
 import 'react-pdf/dist/Page/TextLayer.css';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
-
 import './PdfToPptxConverter.css'; // 自定义样式文件
 
 pdfjs.GlobalWorkerOptions.workerSrc = `/pdf.worker.min.js`;
 
-
 function PdfToPptxConverter() {
+  const [scale, setScale] = useState(4); // 添加 scale 状态
+
   const customUploadRequest = async ({ file, onProgress, onError, onSuccess }) => {
     const arrayBuffer = await file.arrayBuffer();
     const pdf = await pdfjs.getDocument({
@@ -19,21 +19,19 @@ function PdfToPptxConverter() {
       cMapPacked: true,
       data: arrayBuffer
     }).promise;
-    const scale = 10;
-    const firstPage = await pdf.getPage(1);
-    const viewport = firstPage.getViewport({ scale: scale });
 
-    const pptxWidth = viewport.width / 72;
-    const pptxHeight = viewport.height / 72;
-    console.log('pptxWidth', pptxWidth, 'pptxHeight', pptxHeight);
+    let firstPage = await pdf.getPage(1);
+    let viewport = firstPage.getViewport({ scale: scale });
+    let pptxWidth = viewport.width / 72;
+    let pptxHeight = viewport.height / 72;
 
     const pptx = new PptxGenJS();
     pptx.defineLayout({ name: 'PdfLayout', width: pptxWidth, height: pptxHeight });
-    pptx.layout = 'PdfLayout'
+    pptx.layout = 'PdfLayout';
 
     for (let i = 1; i <= pdf.numPages; i++) {
       const page = await pdf.getPage(i);
-      const viewport = page.getViewport({ scale: scale });
+      const viewport = page.getViewport({ scale });
       const canvas = document.createElement('canvas');
       const context = canvas.getContext('2d');
       canvas.height = viewport.height;
@@ -51,22 +49,38 @@ function PdfToPptxConverter() {
       file.url = pptxUrl;
       file.linkProps = { download: file.name.slice(0, -4) + '.pptx' };
       onSuccess();
-    });
+    }).catch(onError);
   };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="container">
+        <Row>
+          <Col span={19}>
+            <Slider
+              min={1}
+              max={20}
+              onChange={(value) => setScale(value)}
+              value={typeof scale === 'number' ? scale : 0}
+            />
+          </Col>
+          <Col span={3}>
+            <InputNumber
+              min={1}
+              max={20}
+              value={scale}
+              onChange={(value) => setScale(value)}
+            />
+          </Col>
+        </Row>
         <Upload.Dragger
           accept=".pdf"
           multiple={true}
           customRequest={customUploadRequest}
-          // maxCount={1}
           style={{ padding: '20px 50px' }}
           progress={{
             type: "line",
             showInfo: true,
-            // percent: process,
             format: (percent) => `${parseFloat(percent.toFixed(2))}%`,
           }}
         >
@@ -75,8 +89,9 @@ function PdfToPptxConverter() {
           </p>
           <p className="ant-upload-text">点击或拖拽上传 PDF 文件转换为 PPTX 文件</p>
         </Upload.Dragger>
+
       </div>
-    </div >
+    </div>
   );
 }
 
